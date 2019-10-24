03a9d723ebe3a6c649c95d57b42da0ea8816ab96
dawoodalo <d.3thmani@gmail.com>
2019-10-24 10:09:51 +0300

    functional

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import ProfileTable from "./ProfileTable";
+import { Redirect } from "react-router-dom";
 
 class Profile extends Component {
   render() {
     if (this.props.loading) return <div>Loading</div>;
     let profile = this.props.profile;
     let user = this.props.user;
-
+    if (!user || !profile) return <Redirect to="/" />;
     return (
       <div className="container">
         <h1>
